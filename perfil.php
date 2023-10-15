4ec81b78b8e4f1c468804c422849b17810d87f29
Aline Bancks <87613356+leidery@users.noreply.github.com>
2023-10-15 17:40:35 -0300

    .

--- perfil.php
+++ perfil.php
@@ -36,7 +36,7 @@
         <div class="offcanvas-body">
           <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
             <li class="nav-item">
-              <a class="nav-link active" class="nav-link" aria-current="page" href="perfil.html">Perfil</a>
+              <a class="nav-link active" class="nav-link" aria-current="page" href="perfil.php">Perfil</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="index.html">Sair</a>
