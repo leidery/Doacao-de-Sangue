f67f1f29eae9dd30b981d59edad8be325148d314
bea <beatriztattooer@gmail.com>
2023-10-19 16:57:40 -0300

    refactor: routes

--- routes/index.php
+++ routes/index.php
@@ -2,7 +2,7 @@
     $projectName = '/Doacao-de-Sangue';
 
     $routes = [
-        $projectName.'/' => 'views/website/pages/home.php',
+        $projectName.'/' => 'views/website/pages/home/index.php',
         $projectName.'/esqueci-a-senha' => 'views/auth/forgot-password/index.php',
         $projectName.'/login' => 'views/auth/login/index.php',
         $projectName.'/cadastrar' => 'views/auth/register/index.php',
