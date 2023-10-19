b722d1c23256d7d0b8a2f4cc2d855b8a791aa5f2
bea <beatriztattooer@gmail.com>
2023-10-19 15:20:55 -0300

    :sparkles: feat[auth]: forgot-password

---
+++ views/auth/forgot-password/content.php
@@ -0,0 +1,15 @@
+<div class="container">
+    <div class="container" id="recSenha">
+        <form>
+            <h2>Recuperar senha</h2>
+            <br>
+            <fieldset>
+                <legend for="exampleInputEmail1">E-mail</legend>
+                <input type="email" id="exampleInputEmail1" class="input-only-text">
+                <br>
+            </fieldset>
+            <button type="submit" class="btn btn-secondary">Enviar código</button>
+            <button type="button" class="btn btn-danger" id="bt-senha"><a href="/login">Código recebido</a></button>
+        </form>
+    </div>
+</div>
---
+++ views/auth/forgot-password/index.php
@@ -0,0 +1,6 @@
+<?php
+    $pageTitle = "Esqueci a senha";
+    $content = file_get_contents('./content.php');
+
+    include '../../website/layout/index.php';
+?>
