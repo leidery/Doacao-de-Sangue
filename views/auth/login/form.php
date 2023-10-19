49226b5111c7155a55d55d1a2d64f0d4c183a5f7
bea <beatriztattooer@gmail.com>
2023-10-19 14:41:17 -0300

    :construction: :recycle: wip: refactor

---
+++ views/auth/login/form.php
@@ -0,0 +1,47 @@
+<form
+    action="controllers/login.php"
+    method="post"
+    id="loginForm"
+    >
+    <legend>Login</legend>
+
+    <fieldset>
+        <label for="email">E-mail</label>
+        <input
+            type="email"
+            id="email"
+            class="input-only-text"
+            name="email"
+            required
+        />
+    </fieldset>
+    
+    <fieldset>
+        <label for="senha">Senha</label>
+        <input
+            type="password"
+            id="exampleInputPassword1"
+            class="input-only-text"
+            name="senha"
+            required
+        />
+    </fieldset>
+    
+    <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+        <label class="form-check-label" for="exampleCheck1">Lembrar-me</label>
+    </div>
+
+    <button
+        type="submit"
+        class="btn btn-danger"
+        style="margin-bottom: 20px;"
+        id="botao-login"
+        name="submit"
+        value="logar"
+    >
+        Entrar
+    </button>
+
+    <a href="esquecisenha.html" id="link-senha">Esqueceu sua senha?</a>
+</form>
