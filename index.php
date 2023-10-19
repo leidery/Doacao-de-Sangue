d1933ab0ccf5dc1d9e8ede6e85a192bc9ecfb4c3
bea <beatriztattooer@gmail.com>
2023-10-19 16:58:16 -0300

    :sparkles: feat: new constants
    constants for pages & public assets

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
     // Global paths:
     define('SITE_ROOT', __DIR__);
+    define('PUBLIC_ASSETS', __DIR__.'/public');
     define('WEBSITE_LAYOUT', __DIR__.'/views/website/layout');
+    define('WEBSITE_PAGES', __DIR__.'/views/website/pages');
 
     // Routes settings:
     include 'routes/index.php';
