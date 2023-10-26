6cdf679e4868b0229227f3be247a8721014cf059
bea <beatriztattooer@gmail.com>
2023-10-26 09:22:59 -0300

    :construction: WIP: profile

--- views/dashboard/editarperfil.php
+++ views/dashboard/editarperfil.php
@@ -12,7 +12,7 @@
     crossorigin="anonymous"></script>
     <link rel="stylesheet"
     href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo SITE_ROOT ?>/public/css/index.css">
     <title>Editar Perfil</title>
 </head>
 <body>
