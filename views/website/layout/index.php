073eb0025a54e481353a62462f9520c1e6a1a13a
bea <beatriztattooer@gmail.com>
2023-10-26 09:22:32 -0300

    fix website layout css import

--- views/website/layout/index.php
+++ views/website/layout/index.php
@@ -18,7 +18,7 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
         
         <!-- Custom CSS -->
-        <link rel="stylesheet" type="text/css" href="<?php echo SITE_ROOT ?>/public/css/style.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo SITE_ROOT ?>/public/css/index.css">
         
         <title>Doação de Sangue | <?php echo $pageTitle; ?></title>
     </head>
