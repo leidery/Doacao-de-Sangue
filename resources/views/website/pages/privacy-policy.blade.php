281e9c4f3cecd92fbfe5bf4bd4ab264065043639
bea <beatriztattooer@gmail.com>
2023-11-20 03:02:51 -0300

    :sparkles: feat[privacy-policy]

---
+++ resources/views/website/pages/privacy-policy.blade.php
@@ -0,0 +1,42 @@
+<x-guest-layout>
+    @include('website.layouts.navbar')
+
+    <x-website.header bgImg="img/bg-header.png">
+        <h1>Política de Privacidade</h1>
+    </x-website.header>
+
+    <div class="container pt-5">
+        <div class="pb-2">
+            <p><strong>ATENÇÃO:</strong></p>
+            <p>
+                Esta política abrange nossas atividades de coleta de dados online dos dados pessoais
+                que coletamos em nossa plataforma.
+            </p>
+        </div>
+
+        <div class="pb-3">
+            <h2>
+            Quais são os dados pessoais coletados?
+            </h2>
+            Os dados pessoais coletados são:
+            <ul>
+                <li>Informações de login</li>
+                <li>Informações de contato</li>
+                <li>Pesquisas de mercado e feedback de consumidores</li>
+                <li>Conteúdo gerado por compartilhamento do próprio usuário</li>
+                <li>Dados pessoais sensíveis</li>
+            </ul>
+        </div>
+
+        <div class="pb-5">
+            <h2>Sobre o uso de cookies</h2>
+            <p>
+                Cookies são utilizados nesta plataforma para melhorar o uso e a funcionalidade do site.
+                Usamos apenas cookies de sessão
+                (aqueles que permanecem no seu navegador até você deletar manualmente).
+            </p>
+        </div>
+    </div>
+
+    @include('website.layouts.footer')
+</x-guest-layout>
