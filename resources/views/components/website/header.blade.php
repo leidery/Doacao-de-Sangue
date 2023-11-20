fc27569739411dc0578151d670b1f46f1d33f3c4
bea <beatriztattooer@gmail.com>
2023-11-20 03:03:29 -0300

    :sparkles: feat[component]: header

---
+++ resources/views/components/website/header.blade.php
@@ -0,0 +1,7 @@
+<section class="header" style="background-image: url({{ $bgImg }});">
+    <div class="container h-100 d-flex">
+        <div class="my-auto">
+            {{ $slot }}
+        </div>
+    </div>
+</section>
