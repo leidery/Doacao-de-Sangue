d9dc66a7b0decc9fbfdf0abf716acd9f36f3c7c0
bea <beatriztattooer@gmail.com>
2023-11-20 03:03:53 -0300

    :fire: burn

--- resources/views/website/content.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="container-md">
-    @include('website.header')
-    @include('website.about-us')
-    @include('website.faq')
-</div>
--- resources/views/welcome.blade.php
+++
@@ -1,5 +0,0 @@
-<x-guest-layout>
-    @include('website.layouts.navbar')
-    @include('website.content')
-    @include('website.layouts.footer')
-</x-guest-layout>
