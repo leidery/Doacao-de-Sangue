417703018c4b963689495fbb410dad1c74a0ca8d
bea <beatriztattooer@gmail.com>
2023-12-03 10:59:53 -0300

    :sparkles: new layouts

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -13,15 +13,10 @@
         
         <!-- Custom CSS -->
         <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
-        <link rel="stylesheet" type="text/css" href="public/css/app.css">
+        <link rel="stylesheet" type="text/css" href="public/css/app.css'">
     </head>
     <body>
-        @include('website.layouts.navbar')
-            
-        {{ $slot }}
-            
-        @include('website.layouts.footer')
-
+        {{ $slot }}  
         <!-- Bootstrap JS -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </body>
