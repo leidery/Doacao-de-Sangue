1eb95a9aad954ba68f28b50b4985852177133f99
bea <beatriztattooer@gmail.com>
2023-12-03 10:59:44 -0300

    WIP: requisitos page

---
+++ resources/views/website/pages/requisitos.blade.php
@@ -0,0 +1,30 @@
+<x-guest-layout>
+    <x-website.header bgImg="{{ asset('img/bg-header.png') }}">
+        <h1>Requisitos para doação</h1>
+    </x-website.header>
+
+    <div class="container py-5">
+        <ul>
+            <li>
+                Apresentar documento oficial com foto
+                (Carteira de Identidade,
+                Carteira do Conselho Profissional,
+                Carteira de Trabalho,
+                Passaporte ou
+                Carteira Nacional de Habilitação)
+            </li>
+
+            <li>Ter entre 16 e 69 anos
+                <ul>
+                    <li>
+                        Lembramos que menores de idade
+                        precisam de autorização
+                        e presença do responsável legal! :D
+                    </li>
+                </ul>
+            </li>
+            <li>Pesar acima de 50 quilos.</li>
+            <li>Estar alimentado e evitar consumo de alimentos gordurosos 3 horas antes da doação</li>
+        </ul>
+    </div>
+</x-guest-layout>
