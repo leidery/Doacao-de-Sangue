36e0b1ed9271f1df190ad51c6f8cbf4ba3e9dfb8
bea <beatriztattooer@gmail.com>
2023-12-04 16:10:11 -0300

    :construction: WIP: tratamento de erros form (front)
    -> dash: busca hemocentro

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
 
             <!-- FORM DE PESQUISA: -->
-            <div class="col-12">
+            <div class="col-12 d-flex">
                 <form
                     action="procurar-hemocentro"
                     method="GET"
@@ -21,6 +21,7 @@
                                     name="cep"
                                     class="form-control"
                                     placeholder="Digite o CEP"
+                                    required
                                 />
                                 <label>Digite aqui o CEP</label>
                             </div>
@@ -32,6 +33,15 @@
                         >
                             Pesquisar
                         </button>
+                        
+                        <button
+                            type="button"
+                            class="btn btn-danger ms-3"
+                        >
+                            <a href="/dashboard" style="text-decoration: none; color: white">
+                                Limpar pesquisa
+                            </a>
+                        </button>
                 </form>
             </div>
 
@@ -41,17 +51,23 @@
                 <p>Exibindo todos os resultados:</p>
             </div>
             @endif
+            @if(count($bloodbanks) < 1 && !$cep)
+                <div class="col-12 mt-2">
+                    <p>Sem bancos de sangue disponíveis.</p>
+                </div>
+            @endif
+
             @if($cep)
             <div class="col-12 mt-5">
                 <p>Exibindo resultados da busca por: <strong>{{ $cep }}</strong></p>
             </div>
             @endif
-            
-            @if(count($bloodbanks) < 1)
+            @if(count($bloodbanks) < 1 && $cep)
                 <div class="col-12 mt-2">
-                    <p>Sem bancos de sangue disponíveis</p>
+                    <p>Sem bancos de sangue disponíveis no CEP informado</p>
                 </div>
             @endif
+
             @foreach ($bloodbanks as $bloodbank)
                 <div class="col-12 mt-2">
                     <x-dashboard.bloodbank-card
