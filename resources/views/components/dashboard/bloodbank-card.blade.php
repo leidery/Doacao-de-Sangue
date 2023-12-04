37f20e514922802e06b6b9c399c39ca0419a81aa
bea <beatriztattooer@gmail.com>
2023-12-04 14:34:58 -0300

    WIP: hemocentro card

---
+++ resources/views/components/dashboard/bloodbank-card.blade.php
@@ -0,0 +1,8 @@
+<div class="d-flex">
+    <img class="bloodbank__photo" src="{{ asset($img) }}"/>
+    <div class="p-2 ps-4">
+        <h2 class="mb-5">{{ $name }}</h2>
+        <p>{{ $description }}</p>
+        <button class="btn btn-danger btn-blood mt-3 px-5">Agendar doação</button>
+    </div>
+</div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,13 +3,18 @@
         <h2>Hemocentros</h2>
 
         <div class="row">
-            <div class="col-12 col-md-6 mt-5 d-flex">
-                <div class="bloodbank__photo" style="background-image: url({{ asset('img/bg-header.png') }})"></div>
-                <div class="p-2 ps-4">
-                    <h2>Nome do hemocentro</h2>
-                    <p>Descrição</p>
-                    <button class="btn btn-danger btn-blood mt-3 px-5">Agendar doação</button>
-                </div>
+            <div class="col-12 mt-5">
+                <x-dashboard.bloodbankcard
+                    name="Hemocentro"
+                    description="
+                    O Centro de Hematologia e Hemoterapia do Paraná (Hemepar) é uma unidade da Secretaria de Estado de Saúde.
+
+                    É responsável pela coleta, armazenamento, processamento, transfusão e distribuição de sangue para 384 hospitais públicos, privados e filantrópicos que atuam em todas as regiões do Paraná.
+
+                    É uma entidade sem fins lucrativos e atende a demanda de fornecimento de sangue e hemoderivados do Estado graças às doações dos voluntários. A doação é o único modo de salvar a vida de milhares de pessoas, pois não há substituto para o sangue.
+                    "
+                    img="img/hemepar.png"
+                />
             </div>
         </div>
     </div>
