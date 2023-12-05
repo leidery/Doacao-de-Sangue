07fc79023aa25eb7fbc96992cb6cd9e0cd6cdd49
bea <beatriztattooer@gmail.com>
2023-12-05 15:19:18 -0300

    :bug: fix: link at cto banner

--- resources/views/website/components/cto-donation.blade.php
+++ resources/views/website/components/cto-donation.blade.php
@@ -2,6 +2,10 @@
     <img src="img/hand-heart.png" alt="">
     <div class="text-center">
         <h3 class="mb-4">Bora salvar vidas?</h3>
-        <button class="btn btn-danger w-100">Agendar doação</button>
+        <a href="/login">
+            <button class="btn btn-danger w-100">
+                Agendar doação
+            </button>
+        </a>
     </div>
 </div>
