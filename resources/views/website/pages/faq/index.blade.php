f76cc5c510939ad029c11736d3a471ae78d5c1ed
bea <beatriztattooer@gmail.com>
2023-12-05 15:12:42 -0300

    :truck: moving: faq

---
+++ resources/views/website/pages/faq/index.blade.php
@@ -0,0 +1,7 @@
+<x-guest-layout>
+    <x-website.header bgImg="{{ asset('img/bg-header.png') }}">
+        <h1>Perguntas Frequentes</h1>
+    </x-website.header>
+
+    @include('website.pages.faq.duvidas')
+</x-guest-layout>
